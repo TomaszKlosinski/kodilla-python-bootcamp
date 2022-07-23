2db713edff960f2fa7d67c9ee520bc830f1edcec
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2022-07-23 18:51:45 +0200

    Add more bonus exercise from Module3

--- Module3/bonus_exercise4/bonus_exercise4.py
+++ Module3/bonus_exercise4/bonus_exercise4.py
@@ -1,9 +1,9 @@
-dice = {x: [] for x in range(2,13)}
+dice = {x: set() for x in range(2,13)}
 
 for i in range(1,7):
     for j in range(1,7):
         sum = i + j
-        dice[sum].append((i, j))
+        dice[sum].add((i, j))
 
 print(dice)
 
