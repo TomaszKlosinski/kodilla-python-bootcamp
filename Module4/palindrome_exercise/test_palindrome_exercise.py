8834c2be089a47110593aa92d92b7f97afe0fc9a
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2022-07-29 15:51:22 +0200

    Refactor palindrome exercise code

--- Module4/palindrome_exercise/test_palindrome_exercise.py
+++ Module4/palindrome_exercise/test_palindrome_exercise.py
@@ -4,13 +4,13 @@ from palindrome import is_palindrome
 
 class TestPalindrome(unittest.TestCase):
 
-    def test_is_paindrome(self):
+    def test_is_paindrome(self) -> None:
         self.assertTrue(is_palindrome('kajak'))
         self.assertTrue(is_palindrome('potop'))
         self.assertTrue(is_palindrome('aka'))
         self.assertTrue(is_palindrome('www'))
 
-    def test_is_not_palindrome(self):
+    def test_is_not_palindrome(self) -> None:
         self.assertFalse(is_palindrome('dom'))
         self.assertFalse(is_palindrome('terminator'))
         self.assertFalse(is_palindrome('alaska'))
