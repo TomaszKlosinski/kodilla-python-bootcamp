df4d15a43b8c81381b7243f4d56eb6fe6b7f4971
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2022-08-31 13:14:02 +0200

    Improve Palindrome exercise

--- Module4/palindrome_exercise/palindrome.py
+++ Module4/palindrome_exercise/palindrome.py
@@ -1,13 +1,4 @@
 def is_palindrome(text: str) -> bool:
     """Function checks if a string is a palindrome"""
 
-    if len(text) == 1:
-        return False
-
-    last_index = len(text) - 1
-
-    for index, letter in enumerate(text):
-        if letter != text[last_index - index]:
-            return False
-
-    return True
+    return text == text[::-1]
