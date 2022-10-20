a5e353905c7d6302e5e733d69a0f86487e4a1651
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2022-10-20 20:47:40 +0200

    Add movies catallogue with tests to Module 14

--- Module14/movies-catalogue-with-tests/tmdb_api_token.py
+++ Module14/movies-catalogue-with-tests/tmdb_api_token.py
@@ -4,5 +4,7 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_token():
-    with open(f'{BASE_DIR}/tmdb_api_token.txt') as f:
-        return f.read()
+    API_TOKEN = os.environ.get("TMDB_API_TOKEN", "")
+    if API_TOKEN == "":
+        with open(f'{BASE_DIR}/tmdb_api_token.txt') as f:
+            return f.read()
